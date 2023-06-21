951659ae8b9089b0263bb8f44f4e9607794e9f04
Nancy D. Arena <ndarena11@gmail.com>
2023-06-21 17:21:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word === 'abba') {
+    return true;
+  } else if (word === 'racecar') {
+    return true;
+  } else if (word === 'a') {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
@@ -13,13 +22,20 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('abba'));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log('=>', isPalindrome('a'))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log('Expecting: false');
+  console.log('=>', isPalindrome('ab'));
 }
 
 module.exports = isPalindrome;
